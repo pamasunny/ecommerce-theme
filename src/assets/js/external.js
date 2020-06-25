904ed32817f4d46528757c1e5128e57f54e3e02a
pamasunny <pamasunny@gmail.com>
2020-06-25 20:53:20 +0530

    [WIP]: added navbar jquery

--- src/assets/js/external.js
+++ src/assets/js/external.js
@@ -1,1 +1,48 @@
 import 'bootstrap';
+import $ from "jquery";
+
+$(document).ready(function() {
+	
+	// variable init
+	var wid;
+	var outer = $("#outer-menu");
+	var out = $("#out-menu");
+
+	// intial function call
+	sizeCheck();
+	
+	// resize global function
+	$(window).resize(sizeCheck);
+	
+	// sizecheck function
+	function sizeCheck(){
+
+	wid = $(window).width();
+	//console.log(wid);
+
+	// bootstap lg size 
+	if (wid < 992 ) {
+	//	console.log("small");
+		outer.hide();
+		
+		out.addClass("navbar-nav d-flex flex-row ");
+		out.html(outer.html());
+	} else {
+		// console.log("large");
+		out.html(out.hide());
+		outer.show().html();
+	}
+	}
+	// console.log("live");
+});
+	
+	
+		
+
+		
+		
+
+	
+
+	 
+
